a208f27cdbd5f0c7d7efa391eb1d3db39a30bf36
Boris Kaul <localvoid@gmail.com>
2018-05-02 13:02:17 +0700

    Refactor build/test configs

--- packages/routekit-resolver/__tests__/resolver.spec.ts
+++ packages/routekit-resolver/__tests__/resolver.spec.ts
@@ -27,134 +27,132 @@ function matchAndMerge(path: string, location: string[]) {
   expect(m!.data! as string[]).toEqual(location);
 }
 
-describe("src/index.ts", function () {
-  describe("static", function () {
-    test("should not match 1", function () {
-      notMatch("/stati");
-    });
-
-    test("should not match 2", function () {
-      notMatch("/static/a/");
-    });
-
-    test("should not match 3", function () {
-      notMatch("/static/b/");
-    });
-
-    test("should match 1", function () {
-      match("/static", "static1");
-    });
-
-    test("should match 2", function () {
-      match("/static/", "static2");
-    });
-
-    test("should match 3", function () {
-      match("/static/a", "static3");
-    });
-
-    test("should match 4", function () {
-      match("/static/b", "static4");
-    });
+describe("static", function () {
+  test("should not match 1", function () {
+    notMatch("/stati");
   });
 
-  describe("param", function () {
-    test("should not match 1", function () {
-      notMatch("/param/");
-    });
-
-    test("should not match 2", function () {
-      notMatch("/param//");
-    });
-
-    test("should not match 3", function () {
-      notMatch("/param/123/a");
-    });
-
-    test("should not match 4", function () {
-      notMatch("/param/123/b/");
-    });
-
-    test("should not match 5", function () {
-      notMatch("/param/123/c/");
-    });
-
-    test("should match 1", function () {
-      match("/param/1", "param1", ["1"]);
-    });
-
-    test("should match 2", function () {
-      match("/param/123", "param1", ["123"]);
-    });
-
-    test("should match 3", function () {
-      match("/param/123/", "param2", ["123"]);
-    });
-
-    test("should match 4", function () {
-      match("/param/123/b", "param3", ["123"]);
-    });
-
-    test("should match 5", function () {
-      match("/param/123/c", "param4", ["123"]);
-    });
+  test("should not match 2", function () {
+    notMatch("/static/a/");
   });
 
-  describe("catchAll", function () {
-    test("should not match 1", function () {
-      notMatch("/catchAll");
-    });
-
-    test("should match 1", function () {
-      match("/catchAll/", "catchAll1", []);
-    });
-
-    test("should match 2", function () {
-      match("/catchAll/1", "catchAll1", ["1"]);
-    });
-
-    test("should match 3", function () {
-      match("/catchAll/123", "catchAll1", ["123"]);
-    });
-
-    test("should match 4", function () {
-      match("/catchAll/123/456", "catchAll1", ["123/456"]);
-    });
+  test("should not match 3", function () {
+    notMatch("/static/b/");
   });
 
-  describe("mix", function () {
-    test("should not match 1", function () {
-      notMatch("/mix");
-    });
-
-    test("should not match 2", function () {
-      notMatch("/mix/1/");
-    });
-
-    test("should match 1", function () {
-      match("/mix/1", "mix1", ["1"]);
-    });
-
-    test("should match 2", function () {
-      match("/mix/1/static", "mix2", ["1"]);
-    });
-
-    test("should match 3", function () {
-      match("/mix/static", "mix3");
-    });
-
-    test("should match 4", function () {
-      match("/mix/static/1", "mix4", ["1"]);
-    });
+  test("should match 1", function () {
+    match("/static", "static1");
   });
 
-  describe("merge data", function () {
-    test("should merge static", function () {
-      matchAndMerge("/static/b", ["start", "home", "static1", "static2", "static4"]);
-    });
+  test("should match 2", function () {
+    match("/static/", "static2");
+  });
 
-    test("should merge param", function () {
-      matchAndMerge("/param/123/b", ["start", "home", "param1", "param2", "param3"]);
-    });
+  test("should match 3", function () {
+    match("/static/a", "static3");
+  });
+
+  test("should match 4", function () {
+    match("/static/b", "static4");
+  });
+});
+
+describe("param", function () {
+  test("should not match 1", function () {
+    notMatch("/param/");
+  });
+
+  test("should not match 2", function () {
+    notMatch("/param//");
+  });
+
+  test("should not match 3", function () {
+    notMatch("/param/123/a");
+  });
+
+  test("should not match 4", function () {
+    notMatch("/param/123/b/");
+  });
+
+  test("should not match 5", function () {
+    notMatch("/param/123/c/");
+  });
+
+  test("should match 1", function () {
+    match("/param/1", "param1", ["1"]);
+  });
+
+  test("should match 2", function () {
+    match("/param/123", "param1", ["123"]);
+  });
+
+  test("should match 3", function () {
+    match("/param/123/", "param2", ["123"]);
+  });
+
+  test("should match 4", function () {
+    match("/param/123/b", "param3", ["123"]);
+  });
+
+  test("should match 5", function () {
+    match("/param/123/c", "param4", ["123"]);
+  });
+});
+
+describe("catchAll", function () {
+  test("should not match 1", function () {
+    notMatch("/catchAll");
+  });
+
+  test("should match 1", function () {
+    match("/catchAll/", "catchAll1", []);
+  });
+
+  test("should match 2", function () {
+    match("/catchAll/1", "catchAll1", ["1"]);
+  });
+
+  test("should match 3", function () {
+    match("/catchAll/123", "catchAll1", ["123"]);
+  });
+
+  test("should match 4", function () {
+    match("/catchAll/123/456", "catchAll1", ["123/456"]);
+  });
+});
+
+describe("mix", function () {
+  test("should not match 1", function () {
+    notMatch("/mix");
+  });
+
+  test("should not match 2", function () {
+    notMatch("/mix/1/");
+  });
+
+  test("should match 1", function () {
+    match("/mix/1", "mix1", ["1"]);
+  });
+
+  test("should match 2", function () {
+    match("/mix/1/static", "mix2", ["1"]);
+  });
+
+  test("should match 3", function () {
+    match("/mix/static", "mix3");
+  });
+
+  test("should match 4", function () {
+    match("/mix/static/1", "mix4", ["1"]);
+  });
+});
+
+describe("merge data", function () {
+  test("should merge static", function () {
+    matchAndMerge("/static/b", ["start", "home", "static1", "static2", "static4"]);
+  });
+
+  test("should merge param", function () {
+    matchAndMerge("/param/123/b", ["start", "home", "param1", "param2", "param3"]);
   });
 });
---
+++ packages/routekit/jest.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  ...require("../jest.config.js"),
+  coverageThreshold: {
+    global: {
+      statements: 100,
+      branches: 100,
+      functions: 100,
+      lines: 100
+    }
+  },
+  testEnvironment: "node",
+};
